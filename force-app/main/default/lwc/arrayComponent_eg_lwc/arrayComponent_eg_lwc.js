fc95b07ce25e4c7a4b6365b31af0181ecc199ebf
Kishore-Katuri <katurikishore37@gmail.com>
2025-11-06 16:10:24 +0530

    arrayComponent_example

---
+++ force-app/main/default/lwc/arrayComponent_eg_lwc/arrayComponent_eg_lwc.js
@@ -0,0 +1,8 @@
+import { LightningElement } from 'lwc';
+
+export default class ArrayComponent_eg_lwc extends LightningElement {
+    name="Array Component";
+    cities=['hyderabad','Chennai','Bangalore','Pune','Mumbai','Delhi'];
+    friends=['Manvitha','Sudeeshna','Harsha','Surya','Ganesh','Nagarjuna','Sreemanth','Bilal','Alekhya','Srikanth'];
+    statesct=[['Hyderabad','Warangal','Karimnagr'],['Amaravathi','Guntur','Vijayawada'],['mumbai','pune','nagpur'],['chennai','madhurai','coimbatore'],['Bengaluru','Manguluru','mysuru'],['jaipur','chandigarh','mohali']];
+}
